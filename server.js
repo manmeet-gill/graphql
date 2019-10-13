1e62e7d35e4d3d92e8d272ebbb9f4d7b5294bf57
Manmeet Gill <msgillus@gmail.com>
2019-10-12 21:08:22 -0700

    server set up

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+const mongoose = require('mongoose')
+
+const graphqlExpress = require('express-graphql')
+const taskSchema = require('./graphql/TaskSchema').TaskSchema
+
+mongoose.connect('mongodb+srv://hackerdojo:passworddojo@cluster0-lgloo.mongodb.net/test?retryWrites=true&w=majority',
+    (err)=>{
+        if(err) throw err;
+        console.log("connected to mongo atlas");
+    }
+);
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use('/graphql', graphqlExpress({
+    schema: taskSchema,
+    rootValue:global,
+    graphiql:true
+}))
+
+app.listen(app.get('port'), () =>{
+    console.log("Node app is running at localhost:"+ app.get('port'))
+})
